a090b144c1ffd198a99318653e15473d25911e4c
brfer98 <bruno98fernandez@gmail.com>
2025-02-23 12:57:56 +0100

    cambiar ruta bbdd
    he cambiado la bbdd que lee la api

--- prueba/conexion.php
+++ prueba/conexion.php
@@ -7,7 +7,8 @@ class Conexion
     public function getConexion()
     {
         try {
-            $this->conection = new PDO("mysql:host=localhost;port=3306;dbname=vivero", "root", "");
+            //$this->conection = new PDO("mysql:host=localhost;port=3306;dbname=vivero", "root", "");
+            $this->conection = new PDO("mysql:host=localhost;port=3306;dbname=vivero-con-login", "root", "");
             $this->conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conection->exec("set names utf8");
         } catch (PDOException $e) {
